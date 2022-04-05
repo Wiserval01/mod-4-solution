eae41e2230e95ece41f983edc9345d74ca5b6039
Rustam Guliyev <101453306+Wiserval01@users.noreply.github.com>
2022-04-05 09:45:04 +0400

    Update script.js
    commit #2

--- JS/script.js
+++ JS/script.js
@@ -29,16 +29,16 @@ WARNING!!! WARNING!!!
 
 (function () {
 
-var names = ["Penelopa", "Johnson", "Jenny", "Brian", "Stewart", "Peter", "Patrick", "Trevor", "Johny", "Joseph"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
 
   if (firstLetter === 'j') {
-    byeSayer(names[i]);
+    byeSpeaker(names[i]);
   }
   else {
-    helloSayer(names[i]);
+    helloSpeaker(names[i]);
   }
 }
 
